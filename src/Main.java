3e93ad5f2a0609c2bbcb9ef525976638ba33a1ab
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-27 12:51:57 -0700

    Created framework and detailed challenge parameters within main.

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/*
+Write a method named area with one double parameter named radius.
+The method needs to return a double value that represents the area of a circle.
+
+If the parameter radius is negative than return -1.0 to represent the invalid value.
+
+Write another overloaded method with 2 parameters x and y (both doubles), where x and y represent the sides of a rectangle.
+The method needs to return the area of a rectangle.
+
+If either or both parameters is negative, return -1
+
+TIP: the formula for calculating the area of a rectangle is X*Y
+TIP: the formula for calculating a circle area is radius * pi
+TIP: for pi, use a constant from the math class, Math.PI
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
